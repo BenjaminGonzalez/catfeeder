6510e771e9e4c6598e4631201eae4890d7b2a9c5
Benjamin Gonzalez <georgeocode110@gmail.com>
2017-07-14 15:50:12 +1000

    Add files via upload

--- CatFeeder/blinkled.py
+++ CatFeeder/blinkled.py
@@ -1,14 +1,26 @@
-import RPi.GPIO as IO        # calling for header file for GPIO’s of PI
-import time                           # calling for time to provide delays in program
-IO.setwarnings(False)          # do not show any warnings
-IO.setmode (IO.BCM)            # programming the GPIO by BCM pin numbers. (like PIN29 as‘GPIO5’)
-IO.setup(19,IO.OUT)             # initialize GPIO19 as an output
-p = IO.PWM(19,50)              # GPIO19 as PWM output, with 50Hz frequency
-p.start(7.5)                             # generate PWM signal with 7.5% duty cycle
-while 1:                                                       # execute loop forever
-        p.ChangeDutyCycle(7.5)                   # change duty cycle for getting the servo position to 90º
-        time.sleep(1)                                      # sleep for 1 second
-        p.ChangeDutyCycle(12.5)                  # change duty cycle for getting the servo position to 180º
-        time.sleep(1)                                     # sleep for 1 second
-        p.ChangeDutyCycle(2.5)                  # change duty cycle for getting the servo position to 0º
-        time.sleep(1)                                     # sleep for 1 second
+# Servo Control
+import time
+import wiringpi
+
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+
+# set #18 to be a PWM output
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+# divide down clock
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+
+delay_period = 0.01
+
+while True:
+    for pulse in range(50, 250, 1):
+        wiringpi.pwmWrite(18, pulse)
+        time.sleep(delay_period)
+    for pulse in range(250, 50, -1):
+        wiringpi.pwmWrite(18, pulse)
+        time.sleep(delay_period)
