b32b0e3aa1ae955be66fd5050bf0030618034bb5
Benjamin Gonzalez <georgeocode110@gmail.com>
2017-07-14 14:53:16 +1000

    Add files via upload

--- CatFeeder/blinkled.py
+++ CatFeeder/blinkled.py
@@ -1,22 +1,14 @@
-import RPi.GPIO as GPIO
-import time
-LedPin = 11    # pin11
-def setup():
-  GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-  GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
-  GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to turn on led
-def blink():
-  while True:
-    GPIO.output(LedPin, GPIO.HIGH)  # led on
-    time.sleep(1)
-    GPIO.output(LedPin, GPIO.LOW) # led off
-    time.sleep(1)
-def destroy():
-  GPIO.output(LedPin, GPIO.LOW)   # led off
-  GPIO.cleanup()                  # Release resource
-if __name__ == '__main__':     # Program start from here
-  setup()
-  try:
-    blink()
-  except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-    destroy()
+import RPi.GPIO as IO        # calling for header file for GPIO’s of PI
+import time                           # calling for time to provide delays in program
+IO.setwarnings(False)          # do not show any warnings
+IO.setmode (IO.BCM)            # programming the GPIO by BCM pin numbers. (like PIN29 as‘GPIO5’)
+IO.setup(19,IO.OUT)             # initialize GPIO19 as an output
+p = IO.PWM(19,50)              # GPIO19 as PWM output, with 50Hz frequency
+p.start(7.5)                             # generate PWM signal with 7.5% duty cycle
+while 1:                                                       # execute loop forever
+        p.ChangeDutyCycle(7.5)                   # change duty cycle for getting the servo position to 90º
+        time.sleep(1)                                      # sleep for 1 second
+        p.ChangeDutyCycle(12.5)                  # change duty cycle for getting the servo position to 180º
+        time.sleep(1)                                     # sleep for 1 second
+        p.ChangeDutyCycle(2.5)                  # change duty cycle for getting the servo position to 0º
+        time.sleep(1)                                     # sleep for 1 second
